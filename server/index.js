44a514d7a073445e95f5699453674e7d52cddf34
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2023-11-23 10:38:21 +0300

    cleanup code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 import http from 'http'
 import {app} from "./routes/index.js"
 import { io } from "./io/index.js";
-import { VehicleType, creations, gameLib, initBases } from './game/main.js';
-import { Base } from './game/index.js';
+import { initBases } from './game/index.js';
 
 const PORT =process.env.PORT || 3000
 const server = http.createServer(app)
@@ -13,10 +12,4 @@ server.listen(PORT, function () {
     console.log('listening on port::' + PORT);
 })
 
-//initBases(50)
-let base = new Base(0,0,{name:"test",alliance:'nullish'})
-gameLib.add(base,VehicleType.BASE)
-console.log(base);
-setInterval(()=>{
-    console.log(creations);
-},200)
+initBases(50)
